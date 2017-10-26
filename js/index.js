b3102d61548dac1178d82fcc7ee6a8b5b3d66ee8
Marichka <svitlananester@ukr.net>
2017-10-26 12:40:59 +0000

    Initial

---
+++ js/index.js
@@ -0,0 +1,7 @@
+document.getElementById("close-table").onclick=function(){
+    document.getElementById("oncl-close").style.display="none";
+};
+document.getElementById('create-g').onclick=function(){
+  document.getElementById("oncl-close").style.display="block";
+};
+    
